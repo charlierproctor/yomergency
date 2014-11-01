79919ab47fbe31fcc9c7e1c8d7440cd52c4ec542
charlierproctor <charlie.proctor@yahoo.com>
2014-10-31 23:13:27 -0400

    working on yo api calls

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var yoall = require('../scripts/yo.js')
+var yo = require('../scripts/yo.js')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	yoall("http://www.google.com");
+	yo("charlierproctor", "http://www.google.com");
   	res.render('index', { title: 'Express' });
 });
 
--- scripts/yo.js
+++ scripts/yo.js
@@ -1,31 +1,39 @@
 var api_token = "25bf5243f31028ba2df7b75cb4189b672ec8baca";
 
-var yoApiCall = function(method, route, addtlParams, callback){
-	var http = new XMLHttpRequest();
+var yoApiCall = function(method, route, params, callback){
+
+	var request = require('request');
 	var url = "https://api.justyo.co/" + route;
-	var params = "api_token=" + api_token + "&" + addtlParams;
-	http.open(method, url, true);
+	console.log(method + ": " + url);
+	console.log(params);
+	request.post(url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    callback(body);
+	  } else{
+	  	console.log("ERROR " + response.statusCode + ": " + body)
+	  }
+	}).form(params)
 
-	//Send the proper header information along with the request
-	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	http.setRequestHeader("Content-length", params.length);
-	http.setRequestHeader("Connection", "close");
-
-	http.onreadystatechange = function() {//Call a function when the state changes.
-	    if(http.readyState == 4 && http.status == 200) {
-	        callback(http.responseText);
-	    }
-	}
-	http.send(params);
 }
 
 var yoall = function(link){
+	var params = {api_token:api_token}
 	if(link){
-		linkParams = "link=" + link
+		params.link = link;
 	}
-	yoApiCall("POST", "yoall", linkParams, function(response){
+	yoApiCall("POST", "yoall", params, function(response){
 		console.log(response);
 	})
 }
 
-module.exports = yoall;
+var yo = function(username,link){
+	var params = {username:username, api_token:api_token}		
+	if(link){
+		params.link = link;
+	}
+	yoApiCall("POST", "yo", params, function(response){
+		console.log(response);
+	})
+}
+
+module.exports = yo;
