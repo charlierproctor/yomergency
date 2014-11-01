261eb4c85f7c71085d4f5567bfd1d92decd5ae5b
charlierproctor <charlie.proctor@yahoo.com>
2014-11-01 03:35:01 -0400

    unnecessary ensureAuthenticated

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,19 @@ var yo = require('../scripts/yo.js')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	if(req.user){
+	if(req.isAuthenticated){
 		res.redirect('/index');
 	} else{
 		res.redirect('/login');		
 	}
 });
 
-router.get('/index', ensureAuthenticated, function(req, res){
-	if(req.user){
+router.get('/index', function(req, res){
+	if(req.isAuthenticated){
 		res.render('index', { user:req.user })
 	} else{
 		res.redirect('/login');		
 	}
 });
-
-var ensureAuthenticated = function(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
-}
 
 module.exports = router;
