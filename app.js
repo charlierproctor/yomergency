fcc0331e98ca9e5c34d85d1f42f20ecbbcc413f0
charlierproctor <charlie.proctor@yahoo.com>
2014-11-01 07:15:10 -0400

    playing with cookies

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
+app.use(cookieParser());
+app.use(bodyParser());
 app.use(session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(logger('dev'));
 
 app.use('/', index);
 app.use('/login', login);
