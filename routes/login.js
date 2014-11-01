18ad11cabbd8c52d45578e0a66b6cca43c8ea93a
charlierproctor <charlie.proctor@yahoo.com>
2014-11-01 00:06:00 -0400

    more fb passport code

--- routes/login.js
+++ routes/login.js
@@ -18,7 +18,20 @@ passport.use(new FacebookStrategy({
 ));
 
 router.get('/', function(req,res){
-
+  res.send("/login");
 });
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+router.get('/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+router.get('/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
 
 module.exports = router;
